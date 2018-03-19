816a1cebfdba0f716d048002d74b7242e9d69e51
Oskar Jakieła <oskar@jakiela.pl>
2018-03-19 11:32:22 +0100

    Display cities list

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { City } from './../city';
+import { CITIES } from './../mock-cities';
 
 @Component({
   selector: 'app-cities',
@@ -7,14 +8,16 @@ import { City } from './../city';
   styleUrls: ['./cities.component.css']
 })
 export class CitiesComponent implements OnInit {
-  city: City = {
-    id: 2643743,
-    name: 'London',
-  };
+  cities = CITIES;
+
+  selectedCity: City;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(city: City): void {
+    this.selectedCity = city;
+  }
 }
---
+++ src/app/mock-cities.ts
@@ -0,0 +1,9 @@
+import { City } from './city';
+
+export const CITIES: City[] = [
+  { id: 2643743, name: 'London' },
+  { id: 2950159, name: 'Berlin' },
+  { id: 3117735, name: 'Madrid' },
+  { id: 3169070, name: 'Roma' },
+  { id: 2988507, name: 'Paris' }
+];
